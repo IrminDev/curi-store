684cec271aadcc1fc3b310cc9e1318422a713681
IrminDev <ihernandezj1902@alumno.ipn.mx>
2024-05-05 22:26:57 -0600

    Test commit

--- curistore-backend/app/Http/Controllers/Api/AddressController.php
+++ curistore-backend/app/Http/Controllers/Api/AddressController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Address index'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json([
+            'message' => 'Address store'
+        ]);
+    }
+
+    public function show($id)
+    {
+        return response()->json([
+            'message' => 'Address show'
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response()->json([
+            'message' => 'Address update'
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json([
+            'message' => 'Address destroy'
+        ]);
+    }
 }
