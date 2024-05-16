11bd18ecbda4d905ddf63f51efa54fa85692dc22
IrminDev <ihernandezj1902@alumno.ipn.mx>
2024-05-16 11:11:01 -0600

    Fixed routes issues

--- curistore-frontend/src/index.js
+++ curistore-frontend/src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import {AdminHome, UserHome} from './views/Home';
 import Login from './views/Login';
 import SignUp from './views/SignUp';
-import Index from './views/user/Index';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,18 +13,8 @@ root.render(
         <Router>
             <Routes>
                 <Route index element={<App />} />
-                <Route path="admin" element={<AdminHome />} >
-                    <Route index element={<p>Inicio</p>} />
-                    <Route path="product-form" element={<p>Productos</p>} />
-                    <Route path="user-form" element={<p>Usuarios</p>} />
-                    <Route path="profile" element={<p>Perfil</p>} />
-                </Route>
-                <Route path="/user" element={<UserHome />}>
-                    <Route index element={<Index/>} />
-                    <Route path="cart" element={<p>Carrito</p>} />
-                    <Route path="orders" element={<p>Compras</p>} />
-                    <Route path="profile" element={<p>Perfil</p>} />
-                </Route>
+                <Route path="admin/*" element={<AdminHome />} />
+                <Route path="user/*" element={<UserHome />} />
                 <Route path="login" element={<Login />} />
                 <Route path="signup" element={<SignUp />} />
             </Routes>
--- curistore-frontend/src/views/Home.jsx
+++ curistore-frontend/src/views/Home.jsx
@@ -6,19 +6,25 @@ import { FaTags } from "react-icons/fa";
 import { FaUsers } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
-import { Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Index from './user/Index';
 
 const AdminHome = () => {
     return (
         <div>
             <Header>
-                <HeaderLink url={'/'} text={'Inicio'}><FaHome/></HeaderLink>
-                <HeaderLink url={'/product-form'} text={'Productos'}><FaTags/></HeaderLink>
-                <HeaderLink url={'/user-form'} text={'Usuarios'}><FaUsers/></HeaderLink>
-                <HeaderLink url={'/profile'} text={'Perfil'}><FaUser/></HeaderLink>
+                <HeaderLink url={'./'} text={'Inicio'}><FaHome/></HeaderLink>
+                <HeaderLink url={'./product-form'} text={'Productos'}><FaTags/></HeaderLink>
+                <HeaderLink url={'./user-form'} text={'Usuarios'}><FaUsers/></HeaderLink>
+                <HeaderLink url={'./profile'} text={'Perfil'}><FaUser/></HeaderLink>
             </Header>
 
-            <Outlet />
+            <Routes>
+                <Route index element={<p>Inicio</p>} />
+                <Route path="product-form" element={<p>Productos</p>} />
+                <Route path="user-form" element={<p>Usuarios</p>} />
+                <Route path="profile" element={<p>Perfil</p>} />
+            </Routes>
         </div>
     )
 }
@@ -33,7 +39,12 @@ const UserHome = () => {
                 <HeaderLink url={'./profile'} text={'Perfil'}><FaUser/></HeaderLink>
             </Header>
 
-            <Outlet />
+            <Routes>
+                <Route index element={<Index/>} />
+                <Route path="cart" element={<p>Carrito</p>} />
+                <Route path="orders" element={<p>Compras</p>} />
+                <Route path="profile" element={<p>Perfil</p>} />
+            </Routes>
         </div>
     )
 }
